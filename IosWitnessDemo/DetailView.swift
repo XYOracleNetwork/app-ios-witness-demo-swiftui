e93597b3393b9d2dff3740b7b087ffa30ab96103
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-11-13 09:26:25 -0600

    Add DetailView Preview for dev

--- IosWitnessDemo/DetailView.swift
+++ IosWitnessDemo/DetailView.swift
@@ -1,11 +1,6 @@
-//
-//  DetailView.swift
-//  IosWitnessDemo
-//
-//  Created by Joel Carter on 11/13/24.
-//
+import SwiftUI
+import SwiftyJSON
 
-import SwiftUICore
 
 
 struct DetailView: View {
@@ -22,5 +17,19 @@ struct DetailView: View {
         .padding()
         .navigationTitle(item.hash)
         .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+
+#Preview("DetailView Preview") {
+    let sampleData = JSON([
+        "schema": "network.xyo.system.info",
+        "_$hash": "e2803afb8d78e33eed1b5c1fbe5bc246d2bdefe80af410abf4e661028ecdc30f",
+        "_hash": "11480c95ebd71e5b9814871c9756bd2e4e63455d331f8b7ed8ee4a3179689212",
+            "_timestamp": 1731465636489
+    ])
+    let sampleItem = Item(json: sampleData)
+    NavigationView {
+        DetailView(item: sampleItem)
     }
 }
