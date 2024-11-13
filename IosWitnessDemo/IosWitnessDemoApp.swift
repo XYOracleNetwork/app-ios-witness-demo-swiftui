649411c0cc293fddf31afe73ed02cd49e1d7acf0
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-11-13 07:59:43 -0600

    Initial commit

---
+++ IosWitnessDemo/IosWitnessDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosWitnessDemoApp.swift
+//  IosWitnessDemo
+//
+//  Created by Joel Carter on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct IosWitnessDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
