6917f34806e1a206da9beacbe621410e62dd83a1
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-11-21 14:41:07 -0600

    Store entire panel witnessed result

--- IosWitnessDemo/ContentView.swift
+++ IosWitnessDemo/ContentView.swift
@@ -4,23 +4,37 @@ import XyoClient
 import SwiftyJSON
 
 let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "network.xyo.IosWitnessDemo", category: "debug")
-let basicWitness = BasicWitness {
-    Payload("network.xyo.basic")
-}
-let systemInfoWitness = SystemInfoWitness(allowPathMonitor: true)
-let locationWitness = LocationWitness()
+
+
 let apiDomain = "https://beta.api.archivist.xyo.network"
 let archive = "Archivist"
-let panel = XyoPanel(
-    archive: archive,
-    apiDomain: apiDomain,
-    witnesses: [basicWitness, systemInfoWitness, locationWitness]
-)
-
-let account = AccountServices.getNamedAccount(name: "default")
 
 struct ContentView: View {
+    private let account: AccountInstance
+    private let panel: XyoPanel
+    
     @State private var payloads: [JsonPayloadItem] = []
+    @State private var previousHash: String?
+    
+    init(account: AccountInstance? = nil) {
+        if let account {
+            self.account = account
+        } else {
+            self.account = AccountServices.getNamedAccount(name: "default")
+        }
+        _previousHash = State(initialValue: self.account.previousHash)
+        let basicWitness = BasicWitness {
+            Payload("network.xyo.basic")
+        }
+        let systemInfoWitness = SystemInfoWitness(allowPathMonitor: true)
+        let locationWitness = LocationWitness()
+        panel = XyoPanel(
+            account: account,
+            witnesses: [basicWitness, systemInfoWitness, locationWitness],
+            archive: archive,
+            apiDomain: apiDomain
+        )
+    }
     
     var body: some View {
         NavigationView {
@@ -50,7 +64,14 @@ struct ContentView: View {
                         Image(systemName: "doc.on.doc")  // Clipboard icon
                             .foregroundColor(.accentColor)
                     }
-//                    .buttonStyle(PlainButtonStyle())
+                }.padding(.horizontal, 16)
+                
+                HStack {
+                    Text("Previous Hash:").bold()
+                    Text("\(previousHash ?? "")")
+                        .lineLimit(1)
+                        .truncationMode(.tail)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                 }.padding(.horizontal, 16)
 
                 Spacer()
@@ -59,9 +80,10 @@ struct ContentView: View {
                 VStack(spacing: 20) {
                     Button("Witness All") {
                         Task {
-                            let result =  await panel.report()
-                            addWitnessedResults(observations: result)
-
+                            let result =  await panel.reportQuery()
+                            let payloads = [result.bw] + result.payloads
+                            addWitnessedResults(observations: payloads)
+                            previousHash = panel.account.previousHash
                         }
                     }
                     .buttonStyle(BorderedButtonStyle())
